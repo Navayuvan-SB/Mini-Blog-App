6f4c2387c7784d57cb0dc8435206ded82151bdca
navayuvansb <sb.navayuvan@gmail.com>
2021-04-08 17:58:09 +0530

    Configure add comment route in urlsConfig

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
     path('blogs/', views.BlogListView.as_view(), name='blogs'),
     path('<int:pk>', views.BlogDetailView.as_view(), name='blog-detail'),
+    path('<int:pk>/create', views.AddCommentView.as_view(), name='add-comment'),
 ]
