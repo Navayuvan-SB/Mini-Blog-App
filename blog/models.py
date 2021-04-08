71228e8ccf1015cce696f4b048a230a6ca6f1b74
navayuvansb <sb.navayuvan@gmail.com>
2021-04-08 11:17:38 +0530

    Define Author Model in blog app

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,16 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Author(models.Model):
     """Model representing an Author"""
 
-    pass
+    name = models.CharField(max_length=100, verbose_name='Author Name')
+    bio = models.TextField('Description about the author')
+    date_of_birth = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("author_detail", kwargs={"pk": self.pk})
