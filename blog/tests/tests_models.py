301d8bf3715cb6830311f444cbde4a00680f2d02
navayuvansb <sb.navayuvan@gmail.com>
2021-04-08 11:10:49 +0530

    Setup test data for Author Model

--- blog/tests/tests_models.py
+++ blog/tests/tests_models.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from blog.models import Author
 
-# Create your tests here.
+
+class AuthorModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        Author.objects.create(
+            name='Sam Willams', bio='A Sample bio with simple description', date_of_birth='11/06/2000')
