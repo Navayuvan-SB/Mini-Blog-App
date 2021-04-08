910c6b47e63d37404f02e964922ddb364c5b12e9
navayuvansb <sb.navayuvan@gmail.com>
2021-04-08 15:09:12 +0530

    Add `blogs` route to urlsConfig on blog app

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
+    path('blogs/', views.BlogListView.as_view(), name='blogs'),
     path('<int:pk>', views.BlogDetailView.as_view(), name='blog-detail'),
 ]
