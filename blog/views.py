1af89c8fddc0accca0beecf6d37e2c3d03985bf1
navayuvansb <sb.navayuvan@gmail.com>
2021-04-08 12:20:58 +0530

    Add Blog detail generic detail view

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Author
+from .models import Author, Blog
 
 
 class AuthorDetailView(generic.DetailView):
     model = Author
+
+
+class BlogDetailView(generic.DetailView):
+    model = Blog
