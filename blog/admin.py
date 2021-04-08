f2ba12357be421960b2b2cfafabdb1031292a7e5
navayuvansb <sb.navayuvan@gmail.com>
2021-04-08 14:35:26 +0530

    Add Author, Blog model in admin panel view

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Author, Blog, Content, Comment
 
-# Register your models here.
+
+class ContentInline(admin.TabularInline):
+    model = Content
+    extra = 2
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 0
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'blogger', 'post_date')
+    inlines = [ContentInline, CommentInline]
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'date_of_birth')
