fda61c293942e5b73147d6177e14e5b795ef1ff7
navayuvansb <sb.navayuvan@gmail.com>
2021-04-08 13:32:58 +0530

    Add testcase to check the context variable of index view

--- blog/tests/tests_views.py
+++ blog/tests/tests_views.py
@@ -1,8 +1,14 @@
 from django.test import TestCase
 
 
-class CommonViewTest(TestCase):
+class IndexViewTest(TestCase):
 
     def test_root_redirect_to_blog(self):
         response = self.client.get('/')
         self.assertRedirects(response, '/blog/', status_code=301)
+
+    def test_context_has_correct_items(self):
+        response = self.client.get('/blog/')
+
+        self.assertTrue('total_number_of_blogs' in response.context)
+        self.assertTrue('total_number_of_authors' in response.context)
