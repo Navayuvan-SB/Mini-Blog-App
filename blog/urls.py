da5091c8e639b7e65b6a9047f8fb52190d9a1268
navayuvansb <sb.navayuvan@gmail.com>
2021-04-08 12:55:44 +0530

    Add config and view to redirect root route to blog index

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
     path('<int:pk>', views.BlogDetailView.as_view(), name='blog-detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.views import generic
+from django.http import HttpResponse
 from .models import Author, Blog
+
+
+def index(request):
+    return HttpResponse('Index Page of Blog App')
 
 
 class AuthorDetailView(generic.DetailView):
