71bfad295dd8dad0fbba043b9743a718115ece55
navayuvansb <sb.navayuvan@gmail.com>
2021-04-08 12:20:41 +0530

    Add blog-detail route in blog urlsconfig

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import AuthorDetailView
+from .views import AuthorDetailView, BlogDetailView
 
 urlpatterns = [
-    path('author/<int:pk>', AuthorDetailView.as_view(), name='author-detail')
+    path('author/<int:pk>', AuthorDetailView.as_view(), name='author-detail'),
+    path('<int:pk>', BlogDetailView.as_view(), name='blog-detail'),
 ]
