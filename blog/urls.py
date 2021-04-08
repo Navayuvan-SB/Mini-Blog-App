e5536f33466cc0cb90e3549e9aaca578bdd1fb4f
navayuvansb <sb.navayuvan@gmail.com>
2021-04-08 16:00:08 +0530

    Add authors list view route to urlsConfig

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('authors/', views.AuthorListView.as_view(), name='authors'),
     path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
     path('blogs/', views.BlogListView.as_view(), name='blogs'),
     path('<int:pk>', views.BlogDetailView.as_view(), name='blog-detail'),
