524e8cfb71f6e8a6b35fbda8d124932f851987b2
navayuvansb <sb.navayuvan@gmail.com>
2021-04-13 18:14:17 +0530

    Add user as ForeignKey in Author Model

--- blog/models.py
+++ blog/models.py
@@ -6,14 +6,13 @@ from django.utils import timezone
 
 class Author(models.Model):
 
-    name = models.CharField(max_length=100, verbose_name='Author Name')
     bio = models.TextField('Description about the author')
     date_of_birth = models.DateField()
 
-    user = models.ForeignKey(User, on_delete=models.SET_NULL)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
-        return self.name
+        return self.user.first_name
 
     def get_absolute_url(self):
         return reverse("author-detail", kwargs={"pk": self.pk})
